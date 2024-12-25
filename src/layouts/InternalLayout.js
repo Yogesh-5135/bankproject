79b38afceffe91cddd9a749a479751da8962604c
omkar3720 <omkarshinde2728@gmail.com>
2024-12-25 11:25:37 +0530

    add

--- src/layouts/InternalLayout.js
+++ src/layouts/InternalLayout.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ProfileNav from "../templates/ProfileNav";
+import SideNav from "../templates/SideNav";
+import "../styles/InternalLayout.css";
 
 function InternalLayout() {
   const [employee, setEmployee] = useState({});
@@ -13,6 +15,14 @@ function InternalLayout() {
   return (
     <div>
       <ProfileNav data={employee} />
+      <div className="row w-100">
+        <div className="col col-3 side-nav-container bg-secondary">
+          <SideNav userType={employee.userType} />
+        </div>
+        <div className="col col-9 navigation-container">
+          <h1>All Routes render here..</h1>
+        </div>
+      </div>
     </div>
   );
 }
