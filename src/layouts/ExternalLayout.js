bfe07a4eb2fa6a5c8c6d7533725e9165b01eb054
omkar3720 <omkarshinde2728@gmail.com>
2024-12-25 11:35:33 +0530

    add

--- src/layouts/ExternalLayout.js
+++ src/layouts/ExternalLayout.js
@@ -1,32 +1,31 @@
-import React from 'react'
-import Header from '../templates/Header'
-import Footer from '../templates/Footer'
-import { Route, Routes } from 'react-router-dom'
-import About from '../templates/About'
-import '../styles/ExternalLayout.css'
-import OurService from '../templates/OurService'
-import EmiClalculator from '../includes/EmiClalculator'
-import Login from '../includes/Login'
-import RegisterEnquiry from '../pages/enquiry/RegisterEnquiry'
+import React from "react";
+import Header from "../templates/Header";
+import Footer from "../templates/Footer";
+import { Route, Routes } from "react-router-dom";
+import About from "../templates/About";
+import "../styles/ExternalLayout.css";
+import OurService from "../templates/OurService";
+import EmiClalculator from "../includes/EmiClalculator";
+import Login from "../includes/Login";
+import Enquirycom from "../pages/enquiry/Enquirycom";
 
 function ExternalLayout() {
   return (
     <div>
-        <Header/>
-         <div className='navigationContainer'>
-            <Routes>
-            <Route path='/' element={<About/>} />
-                <Route path='/about' element={<About/>} />
-                <Route path='/services' element={<OurService/>} />
-                <Route path='/emi-calculator' element={<EmiClalculator/>} />
-                <Route path='/enquiry' element={<RegisterEnquiry/>}/>
-                <Route path='/sign-in' element={<Login/>} />
-            </Routes>
-         </div>
-         <Footer/>
-
+      <Header />
+      <div className="navigationContainer">
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<OurService />} />
+          <Route path="/emi-calculator" element={<EmiClalculator />} />
+          <Route path="/enquiry" element={<Enquirycom />} />
+          <Route path="/sign-in" element={<Login />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default ExternalLayout
+export default ExternalLayout;
--- src/templates/SideNav.js
+++ src/templates/SideNav.js
@@ -1,43 +1,40 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-function SideNav({userType}) {
-    const prefix='/easyfinance/';
-    const userMenus=[
-       //0 index
-        {
-        ADMIN:[
-            {url:`${prefix}add-employee` , label:'Add Employee'},
-            {url:`${prefix}view-employee` , label:'View Employee'},
-            {url:`${prefix}View-enquiries`, label:'View Enquiries'}   
-        ],
-        CRM:[
-            {url:`${prefix}view-new-enquiries`, label:'Registerd Enquiries'},
-            {url:`${prefix}view-enquiry-feedback`, label:'Feedback Enquiries'}
-        ],
-        OE:[],
-        
-    } 
-    ]
-     const [userRoles,setUserRoles] = useState(userMenus);
-
+function SideNav({ userType }) {
+  const prefix = "/easyfinance/";
+  const userMenus = [
+    //0 index
+    {
+      ADMIN: [
+        { url: `${prefix}add-employee`, label: "Add Employee" },
+        { url: `${prefix}view-employee`, label: "View Employee" },
+        { url: `${prefix}View-enquiries`, label: "View Enquiries" },
+      ],
+      CRM: [
+        { url: `${prefix}view-new-enquiries`, label: "Registerd Enquiries" },
+        { url: `${prefix}view-enquiry-feedback`, label: "Feedback Enquiries" },
+      ],
+      OE: [],
+    },
+  ];
+  const [userRoles, setUserRoles] = useState(userMenus);
 
   return (
     <div>
-        <h1>{userType}</h1>
+      <h1>{userType}</h1>
 
-     <div className='ms-5'>
-     {
-            
-            userType && userRoles[0][userType].map((menu)=>
-             <Link className='btn btn-light w-75 mb-3' to={menu.url}> {menu.label}</Link>
-           )
-               
-           }
-     </div>
-        
+      <div className="ms-5">
+        {userType &&
+          userRoles[0][userType].map((menu) => (
+            <Link className="btn btn-light w-75 mb-3" to={menu.url}>
+              {" "}
+              {menu.label}
+            </Link>
+          ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default SideNav
+export default SideNav;
